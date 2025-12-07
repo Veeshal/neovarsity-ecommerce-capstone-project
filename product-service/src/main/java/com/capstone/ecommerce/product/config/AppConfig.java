f4b70556f0229c6fa16705251488b55364b69737
Vishal Moorthy <vishalmoorthy15@gmail.com>
2025-12-07 16:12:12 +0530

    Added jwt validation for product service.

--- product-service/src/main/java/com/capstone/ecommerce/product/config/AppConfig.java
+++ product-service/src/main/java/com/capstone/ecommerce/product/config/AppConfig.java
@@ -1,21 +1,43 @@
 package com.capstone.ecommerce.product.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @Configuration
 public class AppConfig {
 
+    private final JwtDecoder jwtDecoder;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // TODO: Configure security JWT authentication
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .anyRequest().permitAll());
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(registry -> registry
+                        .requestMatchers(openEndpointPatterns()).permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        http.oauth2ResourceServer(oauth2 ->
+                oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder)));
+
         return http.build();
     }
+
+    private String[] openEndpointPatterns() {
+        return new String[]{
+                "/actuator/**",
+                "/swagger-ui.html",
+                "/swagger-ui/**",
+                "/webjars/**",
+                "/v3/api-docs/**",
+                "/error"
+        };
+    }
+
 }
