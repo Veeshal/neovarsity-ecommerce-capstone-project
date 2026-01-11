ff8d37135b94d6ff846f0393fc60b7044fb8174a
Vishal Moorthy <vishalmoorthy15@gmail.com>
2026-01-11 16:22:37 +0530

    Updated stock deduction in product service.

--- product-service/src/main/java/com/capstone/ecommerce/product/listener/OrderEventListener.java
+++ product-service/src/main/java/com/capstone/ecommerce/product/listener/OrderEventListener.java
@@ -1,5 +1,6 @@
 package com.capstone.ecommerce.product.listener;
 
+import com.capstone.ecommerce.product.entity.Product;
 import com.capstone.ecommerce.product.repository.ProductRepository;
 import com.capstone.ecommerce.product.dto.OrderCompletedEvent;
 import com.capstone.ecommerce.product.dto.ProductDto;
@@ -7,6 +8,8 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.stream.Collectors;
 
 @Component
 public class OrderEventListener {
@@ -21,15 +24,30 @@ public class OrderEventListener {
     @KafkaListener(topics = "${kafka.topic.order-completed}", groupId = "product-service")
     @Transactional
     public void handleOrderCompleted(OrderCompletedEvent event) {
-        for (ProductDto item : event.orderItems()) {
-            Long productId = item.id();
-            int quantity = item.quantity();
-            productRepository.findById(productId).ifPresent(product -> {
-                int newQty = product.getQuantity() - quantity;
-                product.setQuantity(newQty);
-//                product.setQuantity(Math.max(newQty, 0));
-                productRepository.save(product);
-            });
+
+        var productIds = event.orderItems().stream().map(ProductDto::id).toList();
+
+        var products = productRepository.findAllById(productIds);
+        var productMap = products.stream()
+                .collect(Collectors.toMap(Product::getId, product -> product));
+
+        boolean isStockSufficient = event.orderItems().stream().allMatch(item -> {
+            Product product = productMap.get(item.id());
+            return product != null && product.getQuantity() >= item.quantity();
+        });
+
+        if (!isStockSufficient) {
+            throw new RuntimeException("Insufficient stock for one or more products in the order");
         }
+
+        // Deduct stock quantities
+        event.orderItems().forEach(item -> {
+            Product product = productMap.get(item.id());
+            if (product != null) {
+                product.setQuantity(product.getQuantity() - item.quantity());
+                productRepository.save(product);
+            }
+        });
+
     }
 }
